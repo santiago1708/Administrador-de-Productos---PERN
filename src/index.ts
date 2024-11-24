8d696e5075987ac392487e170029f2306d827dd6
santiago1708 <vela123tique@gmail.com>
2024-11-24 16:47:45 -0500

    Eliminar carpetas innecesarias del repositorio

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import server from "./server";
-import colors from "colors";
-
-const PORT = process.env.PORT || 4000
-
-server.listen(PORT, () => {
-    console.log(colors.cyan.bold.italic(`Server is running on port ${PORT}`));
-})
